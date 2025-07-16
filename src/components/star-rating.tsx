1dd0dbb38e724b6da08a01d54cc2fedd540242a6
Kaori Kawakami <k.kawakami08@gmail.com>
2025-07-16 09:33:30 -0700

    product page layout

---
+++ src/components/star-rating.tsx
@@ -0,0 +1,35 @@
+import { cn } from "@/lib/utils";
+import { StarIcon } from "lucide-react";
+import React from "react";
+
+const MAX_RATING = 5;
+const MIN_RATING = 0;
+
+type Props = {
+  rating: number;
+  className?: string;
+  iconClassName?: string;
+  text?: string;
+};
+
+const StarRating = ({ rating, className, iconClassName, text }: Props) => {
+  const safeRating = Math.max(MIN_RATING, Math.min(rating, MAX_RATING));
+
+  return (
+    <div className={cn("flex items-center gap-x-1", className)}>
+      {Array.from({ length: MAX_RATING }).map((_, index) => (
+        <StarIcon
+          key={index}
+          className={cn(
+            "size-4",
+            index < safeRating ? "fill-black" : "",
+            iconClassName
+          )}
+        />
+      ))}
+      {text && <p>{text}</p>}
+    </div>
+  );
+};
+
+export default StarRating;
--- src/modules/products/ui/components/product-card.tsx
+++ src/modules/products/ui/components/product-card.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { paths } from "@/constants";
+import { formatCurrency } from "@/lib/utils";
 import { StarIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
@@ -36,7 +37,7 @@ const ProductCard = ({
   };
 
   return (
-    <Link href={paths.home()}>
+    <Link href={paths.product(tenantSlug, id)}>
       <div className="hover:shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] transition-shadow border rounded-md bg-white overflow-hidden h-full flex flex-col">
         <div className="relative aspect-square">
           <Image
@@ -71,13 +72,7 @@ const ProductCard = ({
         </div>
         <div className="p-4">
           <div className="relative px-2 py-1 border bg-kk-lime w-fit">
-            <p className="text-sm font-medium">
-              {new Intl.NumberFormat("en-US", {
-                style: "currency",
-                currency: "USD",
-                maximumFractionDigits: 0,
-              }).format(Number(price))}
-            </p>
+            <p className="text-sm font-medium">{formatCurrency(price)}</p>
           </div>
         </div>
       </div>
